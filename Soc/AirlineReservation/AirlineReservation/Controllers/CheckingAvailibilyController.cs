58736565c234a709c48fa9b9aa1dbc09a28398ea
Vũ Lập <56259405+vulap1097@users.noreply.github.com>
2022-03-10 22:08:55 +0700

    upload project

---
+++ Soc/AirlineReservation/AirlineReservation/Controllers/CheckingAvailibilyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineReservation.Controllers
+{
+    public class CheckingAvailibilyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Soc/AirlineReservation/AirlineReservation/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace AirlineReservation.Models
+{
+    public class User
+    {
+
+    }
+}
